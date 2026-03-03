31ad865dbec6aaf0ad08d3bbd1552b11a2558cc8
at-campos <camposabril01@gmail.com>
2026-03-02 19:37:53 -0600

    trash icon now removes from cart

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,16 @@ const header = document.querySelector("header");
 const cartIco = header.lastElementChild;
 const cart = document.querySelector(".cart");
 
+const removeIco = document.querySelectorAll(".remove");
+
 cartIco.addEventListener("click", () => {
     cart.classList.toggle("show");
+})
+
+
+removeIco.forEach(el => {
+    el.addEventListener("click", () => {
+        const elParent = el.parentElement;
+        elParent.remove();
+    })
 })
